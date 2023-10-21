da4a3c3264cb92ac0cb5dee3aab12c08073b6c1b
alberttransuushi <98855552+alberttransuushi@users.noreply.github.com>
2023-10-21 16:35:41 -0400

    Testing some Shaders for Game Jam

--- Assets/Scripts/FlyCamera.cs
+++ Assets/Scripts/FlyCamera.cs
@@ -6,10 +6,10 @@ public class FlyCamera : MonoBehaviour
 
 
 
-    float mainSpeed = 50.0f; //regular speed
-    float shiftAdd = 75.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 500.0f; //Maximum speed when holdin gshift
-    float camSens = 0.15f; //How sensitive it with mouse
+    public float mainSpeed = 50.0f; //regular speed
+    public float shiftAdd = 75.0f; //multiplied by how long shift is held.  Basically running
+    public float maxShift = 500.0f; //Maximum speed when holdin gshift
+    public float camSens = 0.15f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
 
